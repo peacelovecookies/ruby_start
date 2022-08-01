119831f4f25adc0b8f5bb50bbbd16f5fda651491
Pavel Kim <Pavel.Kim@ipotekabank.uz>
2022-08-01 18:24:18 +0500

    add Comments table && add creating new comment for some specific article

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   # delete "/articles/:id", to: "articles#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
